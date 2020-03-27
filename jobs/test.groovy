9e810164f5d267c357abc5d686c1459f9e9b52a7
EC2 Default User <ec2-user@ip-172-31-15-56.us-east-2.compute.internal>
2020-03-27 19:44:04 +0000

    add testgroovy

---
+++ jobs/test.groovy
@@ -0,0 +1,23 @@
+project_name = "jenkins-pipeline-python"
+repo = "git@github.com:GHrenato/test-jenkins-2.git"
+repo_name = "repo"
+
+pipelineJob(project_name) {
+    definition {
+        triggers{
+            scm("H/2 * * * *")
+        }
+
+        cpsScm{
+            scm {
+                git {
+                    remote {
+                        name(repo_name)
+                        url(repo)
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+    }
+}
